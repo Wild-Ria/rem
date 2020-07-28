f501fe8bcbc1b6fcf115f444cc2546e60ff83ae5
Marianna <marianna.shemshura@gmail.com>
2020-07-28 22:16:40 +0300

    added bootstap and form with validation

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'rms-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      userName: new FormControl(null, [Validators.required]),
+      wifiName: new FormControl(null, [Validators.required]),
+      wifiPassword: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    });
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form);
+  }
+}
